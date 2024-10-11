617e1d64fccc9440f1ebdea160ef1d0580a79806
Saucedo <ASaucedo44@cps.k12.il.us>
2024-10-11 14:20:04 -0500

    first commit

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ shortphrase = newphrase[3:len(newphrase)-3]
  # index 3 which is "n" is inclusive
 # index len(newphrase)-3 which is exclusive
 print(shortphrase) #prints "nst"
+
+#collegeboard version [4:len(newphase)-6]
+#explain why len(newphase)-3 = 9-3 = 6
+#Why does it end with 6?
+#because the last index is not included
