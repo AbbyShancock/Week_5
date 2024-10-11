6200f3b0406a6fc16c9babcc0752e9690aac7316
Saucedo <ASaucedo44@cps.k12.il.us>
2024-10-11 14:12:32 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+newphrase = "rainstorm"
+# 01234567 We are using THIS version
+# 123456789 college board version.
+
+shortphrase = newphrase[3:len(newphrase)-3]
+ # index 3 which is "n" is inclusive
+# index len(newphrase)-3 which is exclusive
+print(shortphrase) #prints "nst"
